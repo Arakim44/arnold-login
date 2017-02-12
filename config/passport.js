b5b6746a044cb07bef8d55b5562536e0ece6533c
Ara Kim <ahrakim44@gmail.com>
2017-02-12 16:24:51 -0600

    sequalized login passport

--- config/passport.js
+++ config/passport.js
@@ -1,16 +1,14 @@
 // config/passport.js
 
 // load all the things we need
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 
 // load up the user model
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
-var dbconfig = require('./database');
-var connection = mysql.createConnection(dbconfig.connection);
+var db = require("../model");
 
-connection.query('USE ' + dbconfig.database);
-// expose this function to our app using module.exports
+
 module.exports = function(passport) {
 
     // =========================================================================
@@ -26,9 +24,10 @@ module.exports = function(passport) {
 
     // used to deserialize the user
     passport.deserializeUser(function(id, done) {
-        connection.query("SELECT * FROM users WHERE id = ? ",[id], function(err, rows){
-            done(err, rows[0]);
-        });
+      db.User.findById(id).then(function(user){
+        done(null, user.dataValues);
+      });
+
     });
 
     // =========================================================================
@@ -40,37 +39,32 @@ module.exports = function(passport) {
     passport.use(
         'local-signup',
         new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
-        },
-        function(req, username, password, done) {
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-            connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows) {
-                if (err)
-                    return done(err);
-                if (rows.length) {
-                    return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
-                } else {
-                    // if there is no user with that username
-                    // create the user
-                    var newUserMysql = {
-                        username: username,
-                        password: bcrypt.hashSync(password, null, null)  // use the generateHash function in our user model
-                    };
-
-                    var insertQuery = "INSERT INTO users ( username, password ) values (?,?)";
-
-                    connection.query(insertQuery,[newUserMysql.username, newUserMysql.password],function(err, rows) {
-                        newUserMysql.id = rows.insertId;
-
-                        return done(null, newUserMysql);
-                    });
-                }
-            });
-        })
+                // by default, local strategy uses username and password, we will override with email
+                usernameField: 'username',
+                passwordField: 'password',
+                passReqToCallback: true // allows us to pass back the entire request to the callback
+            },
+            function(req, user, password, done) {
+                db.User.findAll({
+                    where: {
+                        user_name: user
+                    }
+                }).then(function(rows) {
+                    if (rows.length) {
+                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
+                    } else {
+                        // if there is no user with that username
+                        // create the user
+                        var newUserMysql = {
+                            user_name: user,
+                            user_password: bcrypt.hashSync(password, null, null) // use the generateHash function in our user model
+                        };
+                        db.User.create(newUserMysql).then(function(createdUser) {
+                            return done(null, createdUser);
+                        });
+                    }
+                });
+            })
     );
 
     // =========================================================================
@@ -82,26 +76,34 @@ module.exports = function(passport) {
     passport.use(
         'local-login',
         new LocalStrategy({
-            // by default, local strategy uses username and password, we will override with email
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
-        },
-        function(req, username, password, done) { // callback with email and password from our form
-            connection.query("SELECT * FROM users WHERE username = ?",[username], function(err, rows){
-                if (err)
-                    return done(err);
+                // by default, local strategy uses username and password, we will override with email
+                usernameField: 'username',
+                passwordField: 'password',
+                passReqToCallback: true // allows us to pass back the entire request to the callback
+            },
+            function(req, username, password, done) {
+              db.User.findAll({
+                where:{ user_name : username}
+              }).then(function(rows){
+                console.log(rows);
+
                 if (!rows.length) {
-                    return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+                  return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
                 }
 
                 // if the user is found but the password is wrong
-                if (!bcrypt.compareSync(password, rows[0].password))
-                    return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                if (!bcrypt.compareSync(password, rows[0].dataValues.user_password)) {
+                  return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.')); // create the loginMessage and save it to session as flashdata
+                }
+                // // all is well, return successful user
+                return done(null, rows[0].dataValues);
+              })
 
-                // all is well, return successful user
-                return done(null, rows[0]);
-            });
-        })
+
+               // callback with email and password from our form
+
+            })
     );
+
+
 };
--- model/user.js
+++ model/user.js
@@ -1,4 +1,52 @@
-// var Sequelize = require("sequelize");
-// var sequelize = require("../config/connection.js");
-//
-// var
+module.exports = function(sequelize, DataTypes) {
+
+    var User = sequelize.define("User", {
+
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+
+        user_password:{
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len:[1]
+          }
+        }
+
+        // user_gender: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // },
+        // user_age: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // },
+        // user_weight: {
+        //     type: DataTypes.INTEGER,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // },
+        // fitness_goals: {
+        //     type: DataTypes.TEXT,
+        //     allowNull: false,
+        //     validate: {
+        //         len: [1]
+        //     }
+        // }
+    }, {timestamps: false});
+    User.sync();
+    return User;
+}

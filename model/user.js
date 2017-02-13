6ffb00102a2007d558c7b8367e9758a8d08549ff
Ara Kim <ahrakim44@gmail.com>
2017-02-13 00:49:00 -0600

    sequelized with more register information

--- model/user.js
+++ model/user.js
@@ -10,43 +10,62 @@ module.exports = function(sequelize, DataTypes) {
             }
         },
 
-        user_password:{
-          type: DataTypes.STRING,
-          allowNull: false,
-          validate: {
-            len:[1]
-          }
-        }
+        user_password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
 
-        // user_gender: {
-        //     type: DataTypes.STRING,
-        //     allowNull: false,
-        //     validate: {
-        //         len: [1]
-        //     }
-        // },
-        // user_age: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: false,
-        //     validate: {
-        //         len: [1]
-        //     }
-        // },
-        // user_weight: {
-        //     type: DataTypes.INTEGER,
-        //     allowNull: false,
-        //     validate: {
-        //         len: [1]
-        //     }
-        // },
-        // fitness_goals: {
-        //     type: DataTypes.TEXT,
-        //     allowNull: false,
-        //     validate: {
-        //         len: [1]
-        //     }
-        // }
-    }, {timestamps: false});
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+
+        },
+
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+
+        },
+
+
+        user_gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_age: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        user_weight: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        fitness_goals: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        }
+    });
     User.sync();
     return User;
 }
--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,84 +1,126 @@
 // app/routes.js
+var db = require("../model");
+var bcrypt = require('bcrypt-nodejs');
 module.exports = function(app, passport) {
 
-	// =====================================
-	// HOME PAGE (with login links) ========
-	// =====================================
-	app.get('/', function(req, res) {
-		res.render('index'); // load the index.ejs file
-		//
-	});
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    app.get('/', function(req, res) {
+        res.render('index'); // load the index.ejs file
+        //
+    });
 
-	// =====================================
-	// LOGIN ===============================
-	// =====================================
-	// show the login form
-	app.get('/login', function(req, res) {
+    // =====================================
+    // LOGIN ===============================
+    // =====================================
+    // show the login form
+    app.get('/login', function(req, res) {
 
-		// render the page and pass in any flash data if it exists
-		res.render('login', { message: req.flash('loginMessage') });
-	});
+        // render the page and pass in any flash data if it exists
+        res.render('login', {
+            message: req.flash('loginMessage')
+        });
+    });
 
-	// process the login form
-	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/dashboard', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
-            failureFlash : true // allow flash messages
-		}),
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+            successRedirect: '/dashboard', // redirect to the secure profile section
+            failureRedirect: '/login', // redirect back to the signup page if there is an error
+            failureFlash: true // allow flash messages
+        }),
         function(req, res) {
             console.log("hello");
 
             if (req.body.remember) {
-              req.session.cookie.maxAge = 1000 * 60 * 3;
+                req.session.cookie.maxAge = 1000 * 60 * 3;
             } else {
-              req.session.cookie.expires = false;
+                req.session.cookie.expires = false;
             }
-        res.redirect('/');
+            res.redirect('/');
+        });
+
+    // =====================================
+    // SIGNUP ==============================
+    // =====================================
+    // show the signup form
+    app.get('/signup', function(req, res) {
+        // render the page and pass in any flash data if it exists
+        res.render('signup', {
+            message: req.flash('signupMessage')
+        });
     });
 
-	// =====================================
-	// SIGNUP ==============================
-	// =====================================
-	// show the signup form
-	app.get('/signup', function(req, res) {
-		// render the page and pass in any flash data if it exists
-		res.render('signup', { message: req.flash('signupMessage') });
-	});
+    // process the signup form
+    app.post('/signup', function(req, res) {
+			console.log(req.body);
+        db.User.findAll({
+            where: {
+                user_name: req.body.username
+            }
+        }).then(function(rows) {
+            if (rows.length) {
+                // return done(null, false,
+								req.flash('signupMessage', 'That username is already taken.');
+								res.redirect("/signup");
+            } else {
+                // if there is no user with that username
+                // create the user
+                var newUserMysql = {
+                    user_name: req.body.username,
+                    user_password: bcrypt.hashSync(req.body.password, null, null), // use the generateHash function in our user model
+										first_name: req.body.firstname,
+										last_name: req.body.lastname,
+										user_gender: req.body.gender,
+										user_age: req.body.age,
+										user_weight: req.body.weight,
+										fitness_goals: req.body.goals
 
-	// process the signup form
-	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : '/dashboard', // redirect to the secure profile section
-		failureRedirect : '/signup', // redirect back to the signup page if there is an error
-		failureFlash : true // allow flash messages
-	}));
+                };
+                db.User.create(newUserMysql).then(function(createdUser) {
+                    // return done(null, createdUser);
+										req.flash('success_msg', 'You are registered and can now login');
+										res.redirect(307, '/login');
+									});
 
-	// =====================================
-	// PROFILE SECTION =========================
-	// =====================================
-	// we will want this protected so you have to be logged in to visit
-	// we will use route middleware to verify this (the isLoggedIn function)
-	app.get('/dashboard', isLoggedIn, function(req, res) {
-		res.render('dashboard', {
-			user : req.user // get the user out of session and pass to template
-		});
-	});
+								}
+							})
 
-	// =====================================
-	// LOGOUT ==============================
-	// =====================================
-	app.get('/logout', function(req, res) {
-		req.logout();
-		res.redirect('/');
-	});
+
+
+				//
+        // successRedirect: '/dashboard', // redirect to the secure profile section
+        //     failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        //     failureFlash: true // allow flash messages
+    });
+
+// =====================================
+// PROFILE SECTION =========================
+// =====================================
+// we will want this protected so you have to be logged in to visit
+// we will use route middleware to verify this (the isLoggedIn function)
+app.get('/dashboard', isLoggedIn, function(req, res) {
+    res.render('dashboard', {
+        user: req.user // get the user out of session and pass to template
+    });
+});
+
+// =====================================
+// LOGOUT ==============================
+// =====================================
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
 };
 
 // route middleware to make sure
 function isLoggedIn(req, res, next) {
 
-	// if user is authenticated in the session, carry on
-	if (req.isAuthenticated())
-		return next();
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
 
-	// if they aren't redirect them to the home page
-	res.redirect('/');
+    // if they aren't redirect them to the home page
+    res.redirect('/');
 }
